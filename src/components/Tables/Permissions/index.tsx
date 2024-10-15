0e9e1492b8a2ab2bef8008d18fc71e11a19167a1
Josué Munganga <jmunganga039@gmail.com>
2024-10-15 11:25:50 +0100

    worked on permissions table

---
+++ src/components/Tables/Permissions/index.tsx
@@ -0,0 +1,83 @@
+import DeletePermissionButton from "@/components/Forms/Permissions/DeletePermissionButton";
+import { UpdatePermissionButton } from "@/components/Forms/Permissions/UpdatePermissionButton";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Eye } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+type PermissionsProps = {
+  data: any[];
+};
+
+const Permissions: React.FC<PermissionsProps> = ({ data }) => {
+  return (
+    <div className="my-10 rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <div className="max-w-full overflow-x-auto">
+        <table className="w-full table-auto">
+          <thead>
+            <tr className="bg-gray-2 text-left text-sm dark:bg-meta-4">
+              <th className="min-w-[220px] px-4 py-4 font-medium text-black dark:text-white xl:pl-11">
+                Nom
+              </th>
+              <th className="min-w-[120px] px-4 py-4 font-medium text-black dark:text-white">
+                Code
+              </th>
+
+              <th className="min-w-[120px] px-4 py-4 font-medium text-black dark:text-white">
+                Description
+              </th>
+
+              <th className="min-w-[120px] px-4 py-4 font-medium text-black dark:text-white">
+                Rôles
+              </th>
+              <th className="px-4 py-4 font-medium text-black dark:text-white">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item: any, key: number) => (
+              <tr key={key} className="text-sm">
+                <td className="border-b border-[#eee] px-4 py-5 pl-9 dark:border-strokedark xl:pl-11">
+                  <h5 className="font-medium text-black dark:text-white">
+                    {item?.name}
+                  </h5>
+                </td>
+                <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+                  <p className="text-black dark:text-white">{item?.code}</p>
+                </td>
+
+                <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+                  <p className="text-black dark:text-white">
+                    {item?.description}
+                  </p>
+                </td>
+                <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+                  <p className="text-black dark:text-white">Roles here</p>
+                </td>
+                <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+                  <div className="flex items-center space-x-3.5">
+                    <UpdatePermissionButton permission={item} />
+                    <DeletePermissionButton permission={item} />
+                    <Link
+                      href={`/settings/permissions/${item?.id}`}
+                      className={cn(
+                        buttonVariants({ variant: "outline", size: "icon" }),
+                        "border-none hover:text-meta-1",
+                      )}
+                    >
+                      <Eye className="h-5 w-5" />
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Permissions;
