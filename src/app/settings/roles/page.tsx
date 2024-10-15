99d477ffb25ceb4635dffd34de360b338d1b97a4
Josué Munganga <jmunganga039@gmail.com>
2024-10-15 10:22:20 +0100

    worked on some random pages

--- src/app/settings/roles/page.tsx
+++ src/app/settings/roles/page.tsx
@@ -1,21 +1,46 @@
+"use client";
+
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import Image from "next/image";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import { fetcher } from "@/lib/utils";
+import useSWR from "swr";
+import { DataLoader } from "@/components/common/Loader";
+import { EmptyPlaceholder } from "@/components/EmptyPlaceholder";
+import Roles from "@/components/Tables/Roles";
+import { CreateRoleButton } from "@/components/Forms/Roles/CreateRoleButton";
 
-export const metadata: Metadata = {
-  title: "Gérer les rôles d'utilisateurs -  EasyLife Admin",
-  description: "Gérer les rôles d'utilisateurs -  EasyLife Admin",
-};
+const RolesSettings = () => {
+  const { data, isLoading, error } = useSWR("/api/v1/roles", fetcher);
+  const roles = data?.data?.records;
 
-const RoleSettings = () => {
   return (
     <DefaultLayout>
-      <div className="mx-auto max-w-270">
-        <Breadcrumb pageName="Gérer les rôles d'utilisateurs" />
+      <Breadcrumb pageName="Gérer les Rôles" />
+
+      <div className="flex w-full flex-row items-center justify-end">
+        <CreateRoleButton />
+      </div>
+
+      <div className="flex min-h-screen flex-col gap-10">
+        {isLoading ? (
+          <DataLoader />
+        ) : roles?.length > 0 ? (
+          <Roles data={roles} />
+        ) : (
+          <EmptyPlaceholder>
+            <EmptyPlaceholder.Icon />
+            <EmptyPlaceholder.Title>Aucun rôle trouvé</EmptyPlaceholder.Title>
+            <EmptyPlaceholder.Description>
+              Aucun rôle trouvé
+            </EmptyPlaceholder.Description>
+            <CreateRoleButton />
+          </EmptyPlaceholder>
+        )}
       </div>
     </DefaultLayout>
   );
 };
 
-export default RoleSettings;
+export default RolesSettings;
---
+++ src/components/Tables/Roles/index.tsx
@@ -0,0 +1,73 @@
+import DeleteRoleButton from "@/components/Forms/Roles/DeleteRoleButton";
+import { UpdateRoleButton } from "@/components/Forms/Roles/UpdateRoleButton";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Eye } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+type RolesProps = {
+  data: any[];
+};
+
+const Roles: React.FC<RolesProps> = ({ data }) => {
+  return (
+    <div className="my-10 rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <div className="max-w-full overflow-x-auto">
+        <table className="w-full table-auto">
+          <thead>
+            <tr className="bg-gray-2 text-left text-sm dark:bg-meta-4">
+              <th className="min-w-[220px] px-4 py-4 font-medium text-black dark:text-white xl:pl-11">
+                Nom
+              </th>
+              <th className="min-w-[120px] px-4 py-4 font-medium text-black dark:text-white">
+                Code
+              </th>
+
+              <th className="min-w-[120px] px-4 py-4 font-medium text-black dark:text-white">
+                Permissions
+              </th>
+              <th className="px-4 py-4 font-medium text-black dark:text-white">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item: any, key: number) => (
+              <tr key={key} className="text-sm">
+                <td className="border-b border-[#eee] px-4 py-5 pl-9 dark:border-strokedark xl:pl-11">
+                  <h5 className="font-medium text-black dark:text-white">
+                    {item?.name}
+                  </h5>
+                </td>
+                <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+                  <p className="text-black dark:text-white">{item?.code}</p>
+                </td>
+                <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+                  <p className="text-black dark:text-white">Permissions here</p>
+                </td>
+                <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+                  <div className="flex items-center space-x-3.5">
+                    <UpdateRoleButton role={item} />
+                    <DeleteRoleButton role={item} />
+                    <Link
+                      href={`/settings/roles/${item?.id}`}
+                      className={cn(
+                        buttonVariants({ variant: "outline", size: "icon" }),
+                        "border-none hover:text-meta-1",
+                      )}
+                    >
+                      <Eye className="h-5 w-5" />
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Roles;
