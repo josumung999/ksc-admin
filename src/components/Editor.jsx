5bee40e6d1512f1261414e48bf0f957af78d43f8
Josué Munganga <jmunganga039@gmail.com>
2024-10-17 15:24:45 +0100

    Revert "fixing"
    This reverts commit 1591b90f8e6b8a40d4e334eef040479c47715fdc.

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -6,54 +6,52 @@ import { useEffect, useRef } from "react";
 import { EDITOR_CONFIG } from "@/editorjs.config";
 
 const Editor = ({ value, onChange, holder }) => {
-  // const ref = useRef();
+  const ref = useRef();
 
-  // useEffect(() => {
-  //   if (!ref.current) {
-  //     const editor = new EditorJS({
-  //       holder: holder,
-  //       tools: EDITOR_CONFIG,
-  //       placeholder: "Veuillez décrire votre produit",
-  //       data: value,
-  //       async onChange(api, event) {
-  //         const data = await api.saver.save();
-  //         onChange(data);
-  //       },
-  //       i18n: {
-  //         toolNames: {
-  //           Hyperlink: "Link",
-  //         },
-  //         tools: {
-  //           hyperlink: {
-  //             Save: "Salvar",
-  //             "Select target": "Seleziona destinazione",
-  //             "Select rel": "Wählen rel",
-  //           },
-  //         },
-  //       },
-  //     });
+  useEffect(() => {
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: holder,
+        tools: EDITOR_CONFIG,
+        placeholder: "Veuillez décrire votre produit",
+        data: value,
+        async onChange(api, event) {
+          const data = await api.saver.save();
+          onChange(data);
+        },
+        i18n: {
+          toolNames: {
+            Hyperlink: "Link",
+          },
+          tools: {
+            hyperlink: {
+              Save: "Salvar",
+              "Select target": "Seleziona destinazione",
+              "Select rel": "Wählen rel",
+            },
+          },
+        },
+      });
 
-  //     ref.current = editor;
-  //   }
+      ref.current = editor;
+    }
 
-  //   return () => {
-  //     if (ref.current && ref.current.destroy) {
-  //       ref.current.destroy();
-  //     }
-  //   };
-  // }, [holder, onChange, value]);
+    return () => {
+      if (ref.current && ref.current.destroy) {
+        ref.current.destroy();
+      }
+    };
+  }, [holder, onChange, value]);
 
-  // return (
-  //   <div
-  //     id={holder}
-  //     className={classes.editorjs}
-  //     py={4}
-  //     border={"1px solid rgb(190, 195, 224, 0.4)"}
-  //     borderRadius={6}
-  //   />
-  // );
-
-  return <div>Editor</div>;
+  return (
+    <div
+      id={holder}
+      className={classes.editorjs}
+      py={4}
+      border={"1px solid rgb(190, 195, 224, 0.4)"}
+      borderRadius={6}
+    />
+  );
 };
 
 export default Editor;
