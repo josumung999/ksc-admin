39548b2e45461b77281b5029237978ec9224888f
Josué Munganga <jmunganga039@gmail.com>
2024-10-17 15:31:12 +0100

    worked on product description form

--- src/app/manage/products/create/page.tsx
+++ src/app/manage/products/create/page.tsx
@@ -7,6 +7,7 @@ import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import { Card, CardContent } from "@/components/ui/card";
 import ImageDropzone, { ImageData } from "@/components/ImageDropZone";
 import { useState } from "react";
+import ProductInfo from "@/components/Forms/Products/Informations";
 
 const CreateProduct = () => {
   const [images, setImages] = useState<ImageData[]>([]);
@@ -18,8 +19,8 @@ const CreateProduct = () => {
         <div className="flex flex-col gap-4">
           <h1 className="text-2xl font-bold">Description</h1>
           <Card>
-            <CardContent>
-              <p>Description form here</p>
+            <CardContent className="pt-6">
+              <ProductInfo />
             </CardContent>
           </Card>
         </div>
