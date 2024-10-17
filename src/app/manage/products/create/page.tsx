d50012ad5b739e3c78b9f6f0df3617cb7f42859e
Josué Munganga <jmunganga039@gmail.com>
2024-10-17 15:25:15 +0100

    Revert "temporarily got rid of editorjs"
    This reverts commit 6efb2ead72b7da690c1ee475b6a197f02a1d71f8.

--- src/app/manage/products/create/page.tsx
+++ src/app/manage/products/create/page.tsx
@@ -10,7 +10,6 @@ import ImageDropzone, {
 } from "@/components/Forms/Products/ImageDropZone";
 import { useState } from "react";
 import dynamic from "next/dynamic";
-import ProductInfo from "@/components/Forms/Products/Informations";
 
 // const ProductDescription = dynamic(() => import("@/components/Editor"), {
 //   ssr: false,
@@ -32,7 +31,6 @@ const CreateProduct = () => {
                 onChange={setDescription}
                 holder="product-description"
               /> */}
-              <ProductInfo />
             </CardContent>
           </Card>
         </div>
