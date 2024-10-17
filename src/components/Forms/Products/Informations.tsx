ae498ab9739fc34a1fa348433fb23d22c81df56c
Josué Munganga <jmunganga039@gmail.com>
2024-10-17 15:26:51 +0100

    back to reality

---
+++ src/components/Forms/Products/Informations.tsx
@@ -0,0 +1,56 @@
+"use client";
+import AutoForm, { AutoFormSubmit } from "@/components/ui/auto-form";
+import { DependencyType } from "@/components/ui/auto-form/types";
+import { toast } from "@/hooks/use-toast";
+import { AuthStore } from "@/store/authStore";
+import axios from "axios";
+import { useState } from "react";
+import { mutate } from "swr";
+import * as z from "zod";
+
+export default function ProductInfo() {
+  const formSchema = z.object({
+    name: z
+      .string({
+        required_error: "Champ obligatoire",
+      })
+      .min(10, "Le nom du produit est court")
+      .max(100, "Le nom du produit est long")
+      .describe("Nom du produit"),
+    description: z
+      .string()
+      .min(160, "Laescription du produit est courte")
+      .describe("Description du produit"),
+  });
+
+  type FormData = z.infer<typeof formSchema>;
+
+  const { user } = AuthStore.useState();
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <AutoForm
+      // Pass the schema to the form
+      formSchema={formSchema}
+      // You can add additional config for each field
+      // to customize the UI
+      fieldConfig={{
+        description: {
+          fieldType: "textarea",
+        },
+      }}
+
+      // Optionally, define dependencies between fields
+    >
+      {/* 
+      Pass in a AutoFormSubmit or a button with type="submit".
+      Alternatively, you can not pass a submit button
+      to create auto-saving forms etc.
+      */}
+
+      {/*
+      All children passed to the form will be rendered below the form.
+      */}
+    </AutoForm>
+  );
+}
