0874da1c2f2baac644e6dc04a22e444f76387e07
Job Junior <102473673+jobjunior14@users.noreply.github.com>
2024-11-20 20:03:32 +0200

    order > add client ui added

--- src/app/orders/create/page.tsx
+++ src/app/orders/create/page.tsx
@@ -13,6 +13,9 @@ import { clientType } from "@/components/types_interfaces/clientType";
 import SearchDialogClient from "@/components/common/searchBar/order/client";
 import ClientInformations from "@/components/Forms/orders/client/clientInformation";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import OrderClientDetail from "@/components/Sections/orders/clientDisplayDetail";
+import { Edit } from "lucide-react";
+import { Button } from "@/components/ui/button";
 const NewOrder = () => {
   //   const { data, isLoading, error } = useSWR(
   //     "/api/v1/auth/users?page=1&limit=10",
@@ -32,9 +35,22 @@ const NewOrder = () => {
 
       <div className=" flex min-h-screen w-full flex-col justify-between gap-5 md:flex-row">
         <div className="flex flex-col gap-6">
-          <p className="font-bold">Information du client</p>
+          <div className="flex items-center gap-5">
+            <p className="font-bold">Information du client</p>
+            {clientData && (
+              <Button
+                onClick={() => setClientData(null)}
+                className="p-1"
+                variant={"outline"}
+              >
+                {" "}
+                <Edit className="h-5 w-5" />
+              </Button>
+            )}
+          </div>
           <ScrollArea>
-            <ClientInformations setData={setClientData} />
+            {!clientData && <ClientInformations setData={setClientData} />}
+            {clientData && <OrderClientDetail client={clientData} />}
           </ScrollArea>
         </div>
 
---
+++ src/components/Sections/orders/clientDisplayDetail.tsx
@@ -0,0 +1,32 @@
+import { clientType } from "@/components/types_interfaces/clientType";
+
+interface OrderClientDetailProps {
+  client: clientType;
+}
+
+const OrderClientDetail: React.FC<OrderClientDetailProps> = ({ client }) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <p className=" text-black dark:text-white">
+        Nom copmlet:{" "}
+        <span className="font-bold">{client?.fullName.toUpperCase()}</span>
+      </p>
+      <p className=" text-black dark:text-white">
+        Téléphone:{" "}
+        <span className="font-bold"> {client?.phoneNumber ?? "--"}</span>{" "}
+      </p>
+      <p className=" text-black dark:text-white">
+        Adresse: <span className="font-bold">{client?.address ?? " -- "}</span>
+      </p>
+      <p className=" text-black dark:text-white">
+        Email: <span className="font-bold">{client?.email ?? " -- "}</span>
+      </p>
+      <p className=" text-black dark:text-white">
+        Nationalité:{" "}
+        <span className="font-bold">{client?.civility ?? " -- "}</span>{" "}
+      </p>
+    </div>
+  );
+};
+
+export default OrderClientDetail;
