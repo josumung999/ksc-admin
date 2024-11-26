4c6603406ffff155cc51918829e34bba39968ff9
Job Junior <102473673+jobjunior14@users.noreply.github.com>
2024-11-26 14:26:34 +0200

    datepicker updated

---
+++ src/components/ui/date-pickerWithRange.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import * as React from "react";
+import { addDays, format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { DateRange } from "react-day-picker";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+type DatePickerWithRangeProps = React.HTMLAttributes<HTMLDivElement> & {
+  setDateParant: React.Dispatch<React.SetStateAction<DateRange | undefined>>;
+  className?: React.HTMLAttributes<HTMLDivElement>;
+};
+
+const DatePickerWithRange: React.FC<DatePickerWithRangeProps> = ({
+  setDateParant,
+  className,
+}) => {
+  const [date, setDate] = React.useState<DateRange | undefined>({
+    from: new Date(new Date().setDate(new Date().getDate() - 30)),
+    to: addDays(new Date(), 1),
+  });
+
+  React.useEffect(() => {
+    setDateParant(date);
+  }, [date]);
+
+  return (
+    <div className={cn("grid gap-2", className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={"outline"}
+            className={cn(
+              "w-[300px] justify-start text-left font-normal",
+              !date && "text-muted-foreground",
+            )}
+          >
+            <CalendarIcon />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, "LLL dd, y")} -{" "}
+                  {format(date.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Prennez une date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={setDate}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
+
+export default DatePickerWithRange;
