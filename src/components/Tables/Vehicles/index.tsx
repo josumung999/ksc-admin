f4c8de31be14e8852d1539f7e0b51c289040c26f
Josué Munganga <jmunganga039@gmail.com>
2024-11-26 17:47:35 +0100

    deploying

--- src/components/Tables/Vehicles/index.tsx
+++ src/components/Tables/Vehicles/index.tsx
@@ -11,6 +11,7 @@ type VehiclesTableProps = {
 };
 
 const VehiclesTable: React.FC<VehiclesTableProps> = ({ data }) => {
+  // For deployment
   return (
     <div className="my-10 w-full overflow-hidden rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="overflow-x-auto">
