f92ce04be1676b0d638e34ff482a2e51cdb19a6f
Josué Munganga <jmunganga039@gmail.com>
2024-11-27 14:50:47 +0100

    working on it

--- src/app/shipments/drivers/page.tsx
+++ src/app/shipments/drivers/page.tsx
@@ -1,17 +1,42 @@
+"use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import Image from "next/image";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-
-export const metadata: Metadata = {
-  title: "Gérer les chauffeurs -  EasyLife Admin",
-  description: "Gérer les chauffeurs -  EasyLife Admin",
-};
+import useSWR from "swr";
+import { fetcher } from "@/lib/utils";
+import { DataLoader } from "@/components/common/Loader";
+import DriversStatsTable from "@/components/Tables/DriversStats";
+import { EmptyPlaceholder } from "@/components/EmptyPlaceholder";
 
 const Drivers = () => {
+  const { data, isLoading, error } = useSWR(
+    "/api/v1/livraisons/stats/drivers",
+    fetcher,
+  );
+  const stats = data?.data?.records;
+
   return (
     <DefaultLayout>
       <Breadcrumb pageName="Gérer les livreurs" />
+
+      <div className="flex min-h-screen flex-col gap-10">
+        {isLoading ? (
+          <DataLoader />
+        ) : stats?.length > 0 ? (
+          <DriversStatsTable data={stats} />
+        ) : (
+          <EmptyPlaceholder>
+            <EmptyPlaceholder.Icon />
+            <EmptyPlaceholder.Title>
+              Statistiques non disponible
+            </EmptyPlaceholder.Title>
+            <EmptyPlaceholder.Description>
+              Les statistiques de livreurs seront affichées ici
+            </EmptyPlaceholder.Description>
+          </EmptyPlaceholder>
+        )}
+      </div>
     </DefaultLayout>
   );
 };
