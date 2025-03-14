6467fdf0c68b231527d08372039e8112ecc79243
Josué Munganga <jmunganga039@gmail.com>
2025-03-14 20:39:42 +0100

    getting rid of buggy codes

--- src/components/Dashboard/AdminDashboard.tsx
+++ src/components/Dashboard/AdminDashboard.tsx
@@ -134,7 +134,7 @@ const AdminDashboard: React.FC = () => {
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
-        <ChartTwo />
+        {/* <ChartTwo /> */}
         {/* <ChartThree />
         <MapOne />
         <div className="col-span-12 xl:col-span-8">
