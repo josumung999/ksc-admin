0d78ea12a67fe969ed9c3f4cbfd8c630d0e45078
Josué Munganga <jmunganga039@gmail.com>
2025-03-14 20:34:12 +0100

    trying to figure something out

--- src/components/Dashboard/DeliveryDashBoad.tsx
+++ src/components/Dashboard/DeliveryDashBoad.tsx
@@ -1,22 +1,9 @@
 "use client";
-import dynamic from "next/dynamic";
 import React from "react";
-import ChartOne from "../Charts/ChartOne";
-import ChartTwo from "../Charts/ChartTwo";
-import ChatCard from "../Chat/ChatCard";
-import TableOne from "../Tables/TableOne";
 import CardDataStats from "../CardDataStats";
 import useSWR from "swr";
-import { fetcher, formatCurrency, formatNumber } from "@/lib/utils";
+import { fetcher, formatNumber } from "@/lib/utils";
 import { AuthStore } from "@/store/authStore";
-
-const MapOne = dynamic(() => import("@/components/Maps/MapOne"), {
-  ssr: false,
-});
-
-const ChartThree = dynamic(() => import("@/components/Charts/ChartThree"), {
-  ssr: false,
-});
 
 const DeliveryDashboard: React.FC = () => {
   const { user } = AuthStore.useState();
@@ -147,17 +134,6 @@ const DeliveryDashboard: React.FC = () => {
             />
           </svg>
         </CardDataStats>
-      </div>
-
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
-        {/* <ChartThree />
-        <MapOne />
-        <div className="col-span-12 xl:col-span-8">
-          <TableOne />
-        </div>
-        <ChatCard /> */}
       </div>
     </>
   );
