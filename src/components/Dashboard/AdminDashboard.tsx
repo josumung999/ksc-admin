2983c851660586dd7670d55198499e4a8ae48f71
Josué Munganga <jmunganga039@gmail.com>
2025-03-17 14:42:51 +0100

    added some stats for the admin

--- src/components/Dashboard/AdminDashboard.tsx
+++ src/components/Dashboard/AdminDashboard.tsx
@@ -9,6 +9,7 @@ import CardDataStats from "../CardDataStats";
 import useSWR from "swr";
 import { fetcher, formatCurrency, formatNumber } from "@/lib/utils";
 import { DataLoader } from "../common/Loader";
+import { EmptyPlaceholder } from "../EmptyPlaceholder";
 
 const MapOne = dynamic(() => import("@/components/Maps/MapOne"), {
   ssr: false,
@@ -21,15 +22,22 @@ const ChartThree = dynamic(() => import("@/components/Charts/ChartThree"), {
 const AdminDashboard: React.FC = () => {
   const { data, isLoading, error } = useSWR(`/api/v1/stats/admin`, fetcher);
   const stats = data?.data;
+  const {
+    data: managerStatsData,
+    isLoading: isManagerLoading,
+    error: managerError,
+  } = useSWR(`/api/v1/stats/manager`, fetcher);
+  const managerStats = managerStatsData?.data?.stats;
 
-  console.log("Stats: ", stats);
+  console.log("Stats: ", managerStatsData);
 
   return (
     <>
-      {isLoading ? (
+      {isLoading || isManagerLoading ? (
         <DataLoader />
       ) : stats ? (
         <>
+          <h1 className="text-3xl font-bold">Vue d&apos;ensemble</h1>
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
             <CardDataStats
               title="Total ventes"
@@ -136,6 +144,193 @@ const AdminDashboard: React.FC = () => {
               </svg>
             </CardDataStats>
           </div>
+
+          {managerStats ? (
+            <>
+              <h1 className="text-3xl font-bold">Statistiques de Commandes</h1>
+
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
+                <CardDataStats
+                  title="Commandes en attente"
+                  total={formatNumber(managerStats?.draftOrders)}
+                  rate="0.43%"
+                  levelUp
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    width="22"
+                    height="22"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="text-primary"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                </CardDataStats>
+
+                <CardDataStats
+                  title="Commandes confirmées"
+                  total={formatNumber(managerStats?.confirmedOrders)}
+                  rate="0.95%"
+                  levelDown
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    width={24}
+                    height={24}
+                    className=" text-primary"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                </CardDataStats>
+                <CardDataStats
+                  title="Commandes en transit"
+                  total={formatNumber(managerStats?.inTransitOrders)}
+                  rate="4.35%"
+                  levelUp
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    width="22"
+                    height="22"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    className="text-primary"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                    />
+                  </svg>
+                </CardDataStats>
+                <CardDataStats
+                  title="Commandes livrées"
+                  total={formatNumber(managerStats?.deliveredOrders)}
+                  rate="2.59%"
+                  levelUp
+                >
+                  <svg
+                    className="fill-primary dark:fill-white"
+                    width="22"
+                    height="22"
+                    viewBox="0 0 22 22"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M21.1063 18.0469L19.3875 3.23126C19.2157 1.71876 17.9438 0.584381 16.3969 0.584381H5.56878C4.05628 0.584381 2.78441 1.71876 2.57816 3.23126L0.859406 18.0469C0.756281 18.9063 1.03128 19.7313 1.61566 20.3844C2.20003 21.0375 2.99066 21.3813 3.85003 21.3813H18.1157C18.975 21.3813 19.8 21.0031 20.35 20.3844C20.9 19.7656 21.2094 18.9063 21.1063 18.0469ZM19.2157 19.3531C18.9407 19.6625 18.5625 19.8344 18.15 19.8344H3.85003C3.43753 19.8344 3.05941 19.6625 2.78441 19.3531C2.50941 19.0438 2.37191 18.6313 2.44066 18.2188L4.12503 3.43751C4.19378 2.71563 4.81253 2.16563 5.56878 2.16563H16.4313C17.1532 2.16563 17.7719 2.71563 17.875 3.43751L19.5938 18.2531C19.6282 18.6656 19.4907 19.0438 19.2157 19.3531Z"
+                      fill=""
+                    />
+                    <path
+                      d="M14.3345 5.29375C13.922 5.39688 13.647 5.80938 13.7501 6.22188C13.7845 6.42813 13.8189 6.63438 13.8189 6.80625C13.8189 8.35313 12.547 9.625 11.0001 9.625C9.45327 9.625 8.1814 8.35313 8.1814 6.80625C8.1814 6.6 8.21577 6.42813 8.25015 6.22188C8.35327 5.80938 8.07827 5.39688 7.66577 5.29375C7.25327 5.19063 6.84077 5.46563 6.73765 5.87813C6.6689 6.1875 6.63452 6.49688 6.63452 6.80625C6.63452 9.2125 8.5939 11.1719 11.0001 11.1719C13.4064 11.1719 15.3658 9.2125 15.3658 6.80625C15.3658 6.49688 15.3314 6.1875 15.2626 5.87813C15.1595 5.46563 14.747 5.225 14.3345 5.29375Z"
+                      fill=""
+                    />
+                  </svg>
+                </CardDataStats>
+                <CardDataStats
+                  title="Commandes annulées"
+                  total={formatNumber(managerStats?.returnedOrders)}
+                  rate="0.95%"
+                  levelDown
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height={24}
+                    width={24}
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className=" text-primary"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+                    />
+                  </svg>
+                </CardDataStats>
+              </div>
+
+              <h1 className="text-3xl font-bold">Statistiques de Paiements</h1>
+
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
+                <CardDataStats
+                  title="Paiements effectués"
+                  total={formatNumber(managerStats?.donePayments)}
+                  rate="0.95%"
+                  levelDown
+                >
+                  <svg
+                    width={24}
+                    height={24}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className=" text-primary"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                </CardDataStats>
+                <CardDataStats
+                  title="Paiements en attente"
+                  total={formatNumber(managerStats?.pendingPayments)}
+                  rate="0.95%"
+                  levelDown
+                >
+                  <svg
+                    width={24}
+                    height={24}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className=" text-primary"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v7.5m2.25-6.466a9.016 9.016 0 0 0-3.461-.203c-.536.072-.974.478-1.021 1.017a4.559 4.559 0 0 0-.018.402c0 .464.336.844.775.994l2.95 1.012c.44.15.775.53.775.994 0 .136-.006.27-.018.402-.047.539-.485.945-1.021 1.017a9.077 9.077 0 0 1-3.461-.203M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                    />
+                  </svg>
+                </CardDataStats>
+              </div>
+            </>
+          ) : (
+            <EmptyPlaceholder>
+              <EmptyPlaceholder.Icon />
+              <EmptyPlaceholder.Title>
+                Statistiques non disponible
+              </EmptyPlaceholder.Title>
+              <EmptyPlaceholder.Description>
+                Les statistiques de commandes seront affichées ici
+              </EmptyPlaceholder.Description>
+            </EmptyPlaceholder>
+          )}
 
           <div className="mt-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
             <ChartOne />
